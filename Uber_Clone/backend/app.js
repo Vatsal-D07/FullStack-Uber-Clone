d48885231abd8d4b3b1284ca460c8b1a5b106405
Vatsal-D07 <vatsalyadav768@gmail.com>
2024-11-30 14:51:47 +0530

    user registeration done

--- Uber_Clone/backend/app.js
+++ Uber_Clone/backend/app.js
@@ -3,9 +3,19 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const connectDB = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+connectDB();
+
 app.use(cors());
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
   res.send("Hello world");
 });
+
+app.use('/users',userRoutes)
 
 module.exports = app;
