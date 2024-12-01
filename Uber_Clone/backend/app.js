d523f3ccfc26aa9abfac721a3c0ea193a71e412d
Vatsal-D07 <vatsalyadav768@gmail.com>
2024-12-01 14:26:01 +0530

    user authentication done

--- Uber_Clone/backend/app.js
+++ Uber_Clone/backend/app.js
@@ -5,17 +5,19 @@ const cors = require("cors");
 const app = express();
 const connectDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const cookiesParser = require("cookie-parser");
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookiesParser());
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.use('/users',userRoutes)
+app.use("/users", userRoutes);
 
 module.exports = app;
--- Uber_Clone/backend/routes/user.routes.js
+++ Uber_Clone/backend/routes/user.routes.js
@@ -1,26 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
-const userController = require("../controller/user.contoller");
-router.post('/register', [
-  body('fullname.firstname')
-    .isLength({ min: 3 })
-    .withMessage("First name must be at least 3 characters long"),
 
-  body("email").isEmail().withMessage("Invalid email"),
-  body("password")
-    .isLength({ min: 6 })
-    .withMessage("Password must be atleast 6 characters"),
-],userController.registerUser);
-  
-router.post('/login',
+const userController = require("../controller/user.contoller");
+const { authUser } = require("../middleware/auth.middleware");
+
+router.post(
+  "/register",
   [
-    body('email').isEmail().withMessage("Invalid email"),
-    body('password')
-    .isLength({ min: 6 })
-    .withMessage("Password must be atleast 6 characters"),
-    ],
-    userController.loginUser
-)
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters"),
+  ],
+  userController.registerUser
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters"),
+  ],
+  userController.loginUser
+);
+
+router.get("/profile", authUser, userController.getUserProfile);
+
+router.get("/logout", userController.logoutUser);
 
 module.exports = router;
